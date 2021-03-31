2c1c1d1e358c13f29cefc6864c38ee722fefd7a7
watersphere1999 <58969007+watersphere1999@users.noreply.github.com>
2021-03-31 16:56:50 +0800

    Updates

---
+++ static/js/41.3b1dd1fa.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptest0331-ghpages"]=this["webpackJsonptest0331-ghpages"]||[]).push([[41],{1056:function(e,t,n){"use strict";n.r(t);n(0);var s=n(91),i=n(6);t.default=function(){return Object(i.jsxs)("div",{style:{position:"absolute",left:0,top:0,bottom:0,right:0,display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"},children:[Object(i.jsx)("h2",{children:"Landing Page"}),Object(i.jsx)("div",{children:Object(i.jsx)(s.Link,{to:"/home",children:"Home"})})]})}}}]);
+//# sourceMappingURL=41.3b1dd1fa.chunk.js.map
